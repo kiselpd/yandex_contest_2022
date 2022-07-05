0bebe3bbcaa1a37993fd1b7b9cc4ca9ba4c52671
kiselpd <91218284+kiselpd@users.noreply.github.com>
2022-07-05 23:44:18 +0300

    Add files via upload

--- homework/3.cpp
+++ homework/3.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
+#include <algorithm>
 
 bool checkSimpleNumber(size_t number)
 {   
     if(number == 1)
         return false;
 
-    for(int i = 2; i <= number/2; i++) 
-        if((number % i) == 0)
-            return false;
+    if (number == 2 || number == 3)
+        return true;
 
-    return true;
+    if ((number * number) % 24 == 1)
+        return true;
+
+    return false;
 }
 
 size_t NOD(size_t a, size_t b)
@@ -23,4 +26,55 @@ size_t NOD(size_t a, size_t b)
     }
  
     return a + b;
+}
+
+size_t findSimpleNOD(size_t a, size_t b)
+{
+    size_t min = std::min(a, b);
+    size_t max = std::max(a, b);
+
+    size_t max_NOD = 1;
+
+    for (size_t i = max; i > 0; i--)
+    {
+        if(checkSimpleNumber(i))
+        {
+            if(max_NOD < NOD(i * min, max))
+                max_NOD = NOD(i * min, max);
+        }
+    }
+
+    return max_NOD;
+}
+
+int main()
+{
+    size_t count;
+    std::cin>>count;
+    size_t *mas = nullptr;
+
+    try
+    {
+        mas = new size_t[count];
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+
+    for (size_t i = 0; i < count; i++)
+    {
+        size_t a, b;
+
+        std::cin>>a>>b;
+
+        mas[i] = findSimpleNOD(a, b);
+    }
+
+    for (size_t i = 0; i < count; i++)
+        std::cout << mas[i] << std::endl;
+
+    delete[] mas;
+    return 0;
 }
